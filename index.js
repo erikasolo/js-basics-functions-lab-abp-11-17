7731b61b0f8bdad01cee633a08ebd01af586d24d
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-06 16:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (destination){
+  return destination > 42? destination - 42 : 42 - destination;
+  }
+function distanceFromHqInFeet (destination) {
+  distanceFromHqInBlocks(destination * 264);
+}
