d0920ca498a81b32cc710a5b16e3ae535a16ce1a
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-06 16:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function distanceTravelledInFeet(start, destination) {
       return (start - destination) * 264;
   }
 }
+function calculatesFarePrice(start, destination) {
+
+    if (distanceTravelledInFeet(start,destination) < 400) {
+     return 0;
+  }else if (distanceTravelledInFeet(start,destination) >= 400 && distanceTravelledInFeet(start,destination) <= 2000 ) {
+    return (distanceTravelledInFeet(start,destination) * 2) / 100;
+  }else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500) {
+    return 25;
+  }else if (distanceTravelledInFeet(start,destination) > 2500) {
+    return "cannot travel that far";
+  }
+}
