fce449f1a1b75b6e2ccd20876251fa021c1b0242
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-06 16:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 // Code your solution in this file!
-const HQ = 42;
-
- function distanceFromHqInBlocks(location) {
-   let distance = Math.abs(location-HQ);
-   return distance
- }
-
- function distanceFromHqInFeet(location) {
-   return distanceFromHqInBlocks(location) * 264
-}
-
- function distanceTravelledInFeet(start, end) {
-   let distance = Math.abs(end-start) * 264;
-   return distance
 function distanceFromHqInBlocks (destination){
   return destination > 42? destination - 42 : 42 - destination;
   }
