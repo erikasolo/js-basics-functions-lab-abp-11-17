75171e72332492c261492fd4b5ccb532c838e85e
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-06 16:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks (destination){
-  return destination > 42? destination - 42 : 42 - destination;
-  }
-function distanceFromHqInFeet (destination) {
-  distanceFromHqInBlocks(destination * 264);
-}
-function distanceTravelledInFeet (start, destination) {
-  if (destination > start) {
-    return (destination - start) * 264;
-  }else {
-      return (start - destination) * 264;
-  }
-}
-function calculatesFarePrice(start, destination) {
+const blockVertHQ = 42;
+ 
+ function distanceFromHqInBlocks(blockVertStart){
+   return Math.abs(blockVertHQ - blockVertStart);
+ }
+ 
+ function distanceFromHqInFeet(blockVertStart){
+  return distanceFromHqInBlocks(blockVertStart) * 264;
+ }
+ 
+ function distanceTravelledInFeet (blockVertStart, blockVertEnd){
+  return Math.abs(blockVertStart - blockVertEnd) * 264;
+ }
+ 
+ function calculatesFarePrice(blockVertStart, blockVertEnd){
+  const distance = distanceTravelledInFeet (blockVertStart, blockVertEnd);
+ 
+   switch (true) {
+     case distance > 2500:
+       return 'cannot travel that far';
+     case distance > 2000:
+       return 25;
+     case distance > 400:
+       return distance * 0.02;       //first 400 ft are free, but it only passes when you calculate full distance
+     default:
+       return 0;
+   }
+ }
 
-    if (distanceTravelledInFeet(start,destination) < 400) {
-     return 0;
-  }else if (distanceTravelledInFeet(start,destination) >= 400 && distanceTravelledInFeet(start,destination) <= 2000 ) {
-    return (distanceTravelledInFeet(start,destination) * 2) / 100;
-  }else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500) {
-    return 25;
-  }else if (distanceTravelledInFeet(start,destination) > 2500) {
-    return "cannot travel that far";
-  }
-}
